8bc5d9f258bfb5ca16e6f4be17d615c3baa6a80a
Salman A <salmana.ar@gmail.com>
2024-01-04 14:54:21 +0530

    Add and integrate division function

---
+++ src/helpers/divide.ts
@@ -0,0 +1,3 @@
+export default function divide(first: number, second: number) {
+  return first / second;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import add from "./helpers/add";
 import subtract from "./helpers/subtract";
 import multiply from "./helpers/multiply";
+import divide from "./helpers/divide";
 
 const sum = add(1, 2);
 
@@ -13,3 +14,7 @@ console.log({ difference });
 const product = multiply(1, 2);
 
 console.log({ product });
+
+const quotient = divide(1, 2);
+
+console.log({ quotient });
