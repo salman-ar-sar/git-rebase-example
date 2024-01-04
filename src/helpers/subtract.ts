2b0eb45a503c99558c6045fcd366f6e9ff8129be
Salman A <salmana.ar@gmail.com>
2024-01-04 14:44:43 +0530

    Add and integrate subtract function

---
+++ src/helpers/subtract.ts
@@ -0,0 +1,3 @@
+export default function subtract(first: number, second: number) {
+  return first - second;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import add from "./helpers/add";
+import subtract from "./helpers/subtract";
 import multiply from "./helpers/multiply";
 
 const sum = add(1, 2);
 
 console.log({ sum });
+
+const difference = subtract(1, 2);
+
+console.log({ difference });
 
 const product = multiply(1, 2);
 
